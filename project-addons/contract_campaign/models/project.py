cee1a1ceae12646a2d2d6f3607e18a2dc260c573
Omar (Comunitea) <omar@comunitea.com>
2020-07-09 02:41:11 +0200

    [IMP] contract_campaign: Mejora en la obtención de las unidades de marketing desde proyectos

--- project-addons/contract_campaign/models/project.py
+++ project-addons/contract_campaign/models/project.py
@@ -9,6 +9,11 @@ class ProjectProject(models.Model):
     @api.onchange('partner_id')
     def onchange_partner_id_utm_data(self):
         if self.partner_id:
-            self.campaign_id = self.partner_id.campaign_id.id
-            self.source_id = self.partner_id.source_id.id
-            self.medium_id = self.partner_id.medium_id.id
+            lead = self.env['crm.lead'].\
+                search([('partner_id', '=', self.partner_id.id)], limit=1)
+            self.campaign_id = self.partner_id.campaign_id.id or \
+                (lead and lead.campaign_id.id or False)
+            self.source_id = self.partner_id.source_id.id or \
+                (lead and lead.source_id.id or False)
+            self.medium_id = self.partner_id.medium_id.id or \
+                (lead and lead.medium_id.id or False)
