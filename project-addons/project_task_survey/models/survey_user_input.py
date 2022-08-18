e6b78921166cad80ed724bffdef3e3e534e49af3
Vicente <vicente@comunitea.com>
2022-08-18 14:02:06 +0200

    [IMP] project_task_survey: Campo para agrupar las respuestas por equipo y ver el total

--- project-addons/project_task_survey/models/survey_user_input.py
+++ project-addons/project_task_survey/models/survey_user_input.py
@@ -6,6 +6,9 @@ class SurveyUserInput(models.Model):
 
     project_task_id = fields.Many2one('project.task', string='Project task')
     tag_ids = fields.Many2many('project.tags', related="project_task_id.tag_ids", string="Tags")
+    team_tag = fields.Char(
+        string='Team tag',
+        compute='_compute_team_tag', store=True)
 
     def create(self, vals):
         res = super().create(vals)
@@ -13,3 +16,11 @@ class SurveyUserInput(models.Model):
         if 'task_id' in ctx:
             res.project_task_id = ctx.get('task_id', False)
         return res
+    
+    @api.depends('tag_ids')
+    def _compute_team_tag(self):
+        for user_input in self:
+            team_tag = ""
+            for tag in user_input.tag_ids:
+                team_tag += " {}".format(tag.name)
+            user_input.team_tag = team_tag
