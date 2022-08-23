dad1b904dd9d2003d6e7fae77555f6626c70cb3a
Vicente <vicente@comunitea.com>
2022-08-23 10:22:16 +0200

    [IMP] project_task_survey: Calcular promedio al agrupar por campo equipo

--- project-addons/project_task_survey/models/survey_user_input.py
+++ project-addons/project_task_survey/models/survey_user_input.py
@@ -24,3 +24,23 @@ class SurveyUserInput(models.Model):
             for tag in user_input.tag_ids:
                 team_tag += " {}".format(tag.name)
             user_input.team_tag = team_tag
+    
+    @api.model
+    def read_group(
+        self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True
+    ):
+        """Read group customization in order to display the average quizz_score of the group.
+        """
+        res = read_group_res = super().read_group(
+            domain, fields, groupby, offset=offset, limit=limit, orderby=orderby
+        )
+        if groupby and groupby[0] == "team_tag":
+            if 'quizz_score' in fields:
+                for user_input in res:
+                    if '__domain' in user_input:
+                        user_inputs = self.search(user_input['__domain'])
+                        total_quizz_score = 0.0
+                        for record in user_inputs:
+                            total_quizz_score += record.quizz_score
+                        user_input['quizz_score'] = total_quizz_score/len(user_inputs)
+        return res
