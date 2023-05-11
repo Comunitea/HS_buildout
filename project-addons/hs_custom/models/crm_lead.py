c25b7171f47deacfc3a3c81d7de98ab1c80d9c40
omar7r <omar@comunitea.com>
2023-05-12 00:58:32 +0200

    [IMP] hs_custom: Se controla en el caso de no tener teléfono

--- project-addons/hs_custom/models/crm_lead.py
+++ project-addons/hs_custom/models/crm_lead.py
@@ -126,6 +126,9 @@ class CrmLead(models.Model):
                                              [self.user_id.id], self.team_id.id)
                     self.managed = True
                     self.message_post(body="Cupón nuevo", subtype='mail.mt_comment')
+        elif not self.phone and not self.managed:
+            self.action_set_lost()
+            self.managed = True
 
         return self
 
