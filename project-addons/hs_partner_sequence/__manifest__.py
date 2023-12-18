6d187316821a9091d6e8d2b24519ad1181131f48
Omar (Comunitea) <omar@comunitea.com>
2023-12-18 21:45:45 +0100

    [ADD] hs_partner_sequence

---
+++ project-addons/hs_partner_sequence/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'HS Partner Sequence',
+    'version': '1.0',
+    'description': 'HS Partner Sequence',
+    'author': 'Comunitea',
+    'website': 'https://comunitea.com',
+    'license': 'AGPL-3',
+    'category': 'Custom',
+    'contributors': ['Comunitea ', 'Miguel Vázquez <miguel@comunitea.com>'],
+    'depends': [
+        'base_partner_sequence'
+    ],
+    'data': [
+        'data/partner_sequence.xml'
+    ],
+    'auto_install': False,
+    'installable': True,
+}
---
+++ project-addons/hs_partner_sequence/models/partner.py
@@ -0,0 +1,20 @@
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+
+class ReaPartner(models.Model):
+    _inherit = 'res.partner'
+
+
+    @api.multi
+    def _get_next_ref(self, vals=None):
+        if (vals and vals.get('customer', False)) or self.customer:
+            return super(ReaPartner, self)._get_next_ref(vals=vals)
+        else:
+            return self.env['ir.sequence'].next_by_code('res.partner.supplier')
+
+    @api.model
+    def create(self, vals):
+        if not (vals.get('customer', False) or vals.get('supplier', False)) and self._needsRef(vals=vals):
+            raise ValidationError(_('You must select the type of partner'))
+        return super(ReaPartner, self).create(vals)
