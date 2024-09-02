b419d576d4e6d06dc25de79e699554dbc242a09b
miguelvazquez1 <miguel@comunitea.com>
2024-09-02 13:31:17 +0200

    [IMP]google_ads_integration: gclid

--- project-addons/google_ads_integration/__manifest__.py
+++ project-addons/google_ads_integration/__manifest__.py
@@ -14,6 +14,7 @@
     ],
     'data': [
         'views/res_company_views.xml',
+        'views/crm_lead_views.xml',
         'data/campaign.xml'],
     'auto_install': False,
     'application': False,
